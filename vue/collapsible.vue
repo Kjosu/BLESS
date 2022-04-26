dff7b6ff0efd076ced2655b7a2ba648bbb6d0a54
Kjosu <marcel.muehlberg99@gmail.com>
2022-04-26 17:08:08 +0200

    accordion shizzle

---
+++ vue/collapsible.vue
@@ -0,0 +1,27 @@
+<template>
+    <accordion v-bind="$props" :expanded="expanded" @toggle="toggle">
+        <slot />
+    </accordion>
+</template>
+<script>
+import accordion from './accordion';
+
+export default {
+    components: {
+        accordion
+    },
+    props: {
+        initiallyExpanded: Boolean
+    },
+    data() {
+        return {
+            expanded: this.initiallyExpanded
+        };
+    },
+    methods: {
+        toggle() {
+            this.expanded = !this.expanded;
+        }
+    }
+};
+</script>

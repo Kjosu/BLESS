84107d898e58670810d5a4a08393cffa55835fe1
Kjosu <marcel.muehlberg99@gmail.com>
2022-04-26 18:03:27 +0200

    accordion groups

--- vue/collapsible.vue
+++ vue/collapsible.vue
@@ -11,16 +11,35 @@ export default {
         accordion
     },
     props: {
-        initiallyExpanded: Boolean
+        initiallyExpanded: Boolean,
+        group: String
     },
     data() {
         return {
             expanded: this.initiallyExpanded
         };
     },
+    mounted() {
+        document.addEventListener('bless-accordion-group-toggle', this.handleAccordionGroupToggle);
+    },
     methods: {
         toggle() {
             this.expanded = !this.expanded;
+
+            document.dispatchEvent(new CustomEvent('bless-accordion-group-toggle', {
+                detail: {
+                    group: this.group,
+                    sender: this,
+                    expanded: this.expanded
+                }
+            }));
+        },
+        handleAccordionGroupToggle({ detail: { group, sender, expanded } }) {
+            if (group !== this.group || sender === this || !expanded) {
+                return;
+            }
+
+            this.expanded = false;
         }
     }
 };
